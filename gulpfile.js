a41e8f1cd8e19f52ba6ac9451e3fc48d686bf14a
Westerling <a_munz@list.ru>
2018-09-25 10:49:11 +0300

    Настраивает автоматизацию

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require('gulp-plumber');
+var postcss = require("gulp-postcss");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+
+gulp.task("style", function() {
+  gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass({
+      includePaths: require("node-normalize-scss").includePaths
+    }))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("source/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("serve", function() {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("source/js/*");
+  gulp.watch("source/*.html", ["minify"]).on("change", server.reload);
+});
